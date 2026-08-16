73773637a5e6011e7d39d633b15de05157d74639
yuvrajk-dev21 <0808cs241385.ies@ipsacademy.org>
2026-08-16 14:49:50 +0530

    Implement number guessing game in NumberGame.java
    This Java program implements a number guessing game where the player has 5 attempts to guess a randomly generated number between 1 and 100. The game tracks the number of rounds won and allows the player to play again.

---
+++ NumberGame.java
@@ -0,0 +1,69 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Numbergame {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        int roundsWon = 0;
+        char playAgain;
+
+        do {
+            int number = random.nextInt(100) + 1;
+            int maxAttempts = 5;
+            boolean guessed = false;
+
+            System.out.println("\n=== Number Guessing Game ===");
+            System.out.println("Guess a number between 1 and 100");
+            System.out.println("You have " + maxAttempts + " attempts.");
+
+            for (int i = 1; i <= maxAttempts; i++) {
+
+                System.out.print("Attempt " + i + ": ");
+                int guess = sc.nextInt();
+
+                if (guess == number) {
+                    System.out.println("Correct! You guessed the number.");
+                    roundsWon++;
+                    guessed = true;
+                    break;
+                } else if (guess < number) {
+                    System.out.println("Too low!");
+                } else {
+                    System.out.println("Too high!");
+                }
+            }
+
+            if (!guessed) {
+                System.out.println("You lost! The number was: " + number);
+            }
+
+            System.out.print("Play again? (Y/N): ");
+            playAgain = sc.next().charAt(0);
+
+        } while (playAgain == 'Y' || playAgain == 'y');
+
+        System.out.println("Rounds Won: " + roundsWon);
+        sc.close();
+    }
+}
+
+
+Output
+=== Number Guessing Game ===
+Guess a number between 1 and 100
+You have 5 attempts.
+Attempt 1: 25
+Too low!
+Attempt 2: 32
+Too low!
+Attempt 3: 75
+Too high!
+Attempt 4: 65
+Too high!
+Attempt 5: 50
+Too low!
+You lost! The number was: 51
+Play again? (Y/N): 
